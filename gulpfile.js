226ca7cfb19791b75d00f9f0ef9d2a1511cbe3b4
kkAyataka <kk.ayataka@gmail.com>
2020-01-25 22:29:59 +0900

    Add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,15 @@ const gulp = require('gulp');
 gulp.task('default', () => {
   return gulp.src(
     ['./src/**/*.html', './src/**/*.template', './src/**/*.css',
-      './src/**/*.js',],
+      './src/**/*.js'],
     {base: 'src'}
   )
   .pipe(gulp.dest('./out'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./src/**/*.html', gulp.task('default'));
+  gulp.watch('./src/**/*.template', gulp.task('default'));
+  gulp.watch('./src/**/*.css', gulp.task('default'));
+  gulp.watch('./src/**/*.js', gulp.task('default'));
 });
