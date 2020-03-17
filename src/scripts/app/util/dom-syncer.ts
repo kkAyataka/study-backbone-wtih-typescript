03e9d70eefb5c84d49a3b60649b834f0c5f6c328
kkAyataka <kk.ayataka@gmail.com>
2020-03-17 23:24:22 +0900

    Refact for ESLint

--- src/scripts/app/util/dom-syncer.ts
+++ src/scripts/app/util/dom-syncer.ts
@@ -1,124 +1,130 @@
 /**
  * Attribute name of the rid
- * @ignore
  */
-const ATTR_NAME_RID = 'data-domsyncerid';
+const ATTR_NAME_RID = 'data-domsyncer-rid';
+
+/**
+ * Attributes name of the no sync marker
+ */
+const ATTR_NO_SYNC = 'data-domsyncer-no';
 
 /**
  * Managed embedded rid
- * @ignore
  */
-let g_rid: number = 0;
+let rid = 0;
 
-export namespace DOMSyncer {
-  /**
-   * Embed rendering id (rid) to the HTML elements.
-   *
-   * ```html
-   * <div></div> -> <div data-domsyncerid="0">
-   * ```
-   * @param templateText
-   */
-  export function embedRID(templateText: string): string {
-    return templateText.replace(/<([^/%]("(.|\s)*?"|'(.|\s)*?'|<%(.|\s)*?%>|[^"'<])*?)(?<!%)>/g,
-      (m, p1) => `<${p1} ${ATTR_NAME_RID}="${g_rid++}">`);
+
+/** Copies attributes @private */
+function syncAttributes(nowEl: Element, newEl: Element): void {
+  // update existing attributes
+  for (const attr of nowEl.attributes) {
+    const item = newEl.attributes.getNamedItem(attr.name);
+    if (item) {
+      nowEl.setAttribute(attr.name, item.value);
+    } else if (
+      attr.name !== 'id' &&
+      attr.name !== ATTR_NAME_RID) {
+
+      nowEl.removeAttribute(attr.name);
+    }
   }
 
-  /**
-   *
-   * @param nowEl
-   * @param subViewEls
-   * @param newEl
-   */
-  export function sync(nowEl: Element, subViewEls: Element[], newEl: Element): void {
-    syncElements(nowEl, subViewEls, newEl);
+  // Adds new attributes
+  for (const attr of newEl.attributes) {
+    const item = nowEl.attributes.getNamedItem(attr.name);
+    if (!item) {
+      nowEl.setAttribute(attr.name, attr.value);
+    }
+  }
+}
+
+/** Gets a rid from the attributes @private */
+function getRID(el: Element): string | null {
+  const item = el.attributes.getNamedItem(ATTR_NAME_RID);
+  return item ? item.value : null;
+}
+
+/** @private */
+function isNoSync(el: Element): boolean {
+  const item = el.attributes.getNamedItem(ATTR_NO_SYNC);
+  return item ? true : false;
+}
+
+/** Gets a child element by the rid @private */
+function getChildByRID(el: Element, rid: string, offset = 0): {el: Element; index: number} | null {
+  for (let i = offset; i < el.childElementCount; ++i) {
+    if (getRID(el.children[i]) === rid) {
+      return {el: el.children[i], index: i};
+    }
   }
 
-  /** Implementation of the sync function @private */
-  function syncElements(nowEl: Element, subViewEls: Element[], newEl: Element): void {
-    syncAttributes(nowEl, newEl);
+  return null;
+}
 
-    if (nowEl.childElementCount === 0) {
-      if (newEl.childElementCount > 0) {
-        for (let c of newEl.children) {
-          nowEl.appendChild(c);
-        }
+/** Implementation of the sync function @private */
+function syncElements(nowEl: Element, subViewEls: Element[], newEl: Element): void {
+  syncAttributes(nowEl, newEl);
+
+  if (isNoSync(nowEl)) {
+    nowEl.outerHTML = newEl.outerHTML;
+  } else if (nowEl.childElementCount === 0) {
+    if (newEl.childElementCount > 0) {
+      for (const c of newEl.children) {
+        nowEl.appendChild(c);
       }
+    }
 
-      if (nowEl.outerHTML !== newEl.outerHTML) {
-        nowEl.outerHTML = newEl.outerHTML;
-      }
-    } else {
-      let isSame = true;
+    if (nowEl.outerHTML !== newEl.outerHTML) {
+      nowEl.outerHTML = newEl.outerHTML;
+    }
+  } else {
+    for (let i = 0; i < newEl.childElementCount; ++i) {
+      const newC = newEl.children[i];
+      const newRID = getRID(newC);
 
-      for (let i = 0; i < newEl.childElementCount; ++i) {
-        const newC = newEl.children[i];
-        const newRID =  getRID(newC);
+      if (newRID) {
+        const child = getChildByRID(nowEl, newRID, i);
+        const nowC = child?.el;
+        const nowI = child?.index;
 
-        if (newRID) {
-          const child = getChildByRID(nowEl, newRID, i);
-          const nowC = child?.el;
-          const nowI = child?.index;
-
-          if (nowC) {
-            if (!subViewEls.includes(newC)) {
-              syncElements(nowC, subViewEls, newC);
-            }
-          } else {
-            nowEl.insertBefore(newC, nowEl.children[i]);
-          }
-
-          if (nowC && nowI !== i) {
-            // nowEl.children[i] is none in now
-            nowEl.children[i].remove();
+        if (nowC) {
+          if (!subViewEls.includes(newC)) {
+            syncElements(nowC, subViewEls, newC);
           }
         } else {
           nowEl.insertBefore(newC, nowEl.children[i]);
         }
+
+        if (nowC && nowI !== i) {
+          // nowEl.children[i] is none in now
+          nowEl.children[i].remove();
+        }
+      } else {
+        nowEl.insertBefore(newC, nowEl.children[i]);
       }
     }
-  }
-
-  /** Copies attributes @private */
-  function syncAttributes(nowEl: Element, newEl: Element) {
-    // update existing attributes
-    for (let attr of nowEl.attributes) {
-      const item = newEl.attributes.getNamedItem(attr.name);
-      if (item) {
-        nowEl.setAttribute(attr.name, item.value);
-      } else if (
-        attr.name !== 'id' &&
-        attr.name !== ATTR_NAME_RID) {
-
-        nowEl.removeAttribute(attr.name);
-      }
-    }
-
-    // Adds new attributes
-    for (let attr of newEl.attributes) {
-      const item = nowEl.attributes.getNamedItem(attr.name);
-      if (!item) {
-        nowEl.setAttribute(attr.name, attr.value);
-      }
-    }
-  }
-
-  /** Gets a rid from the attributes @private */
-  function getRID(el: Element): string | null {
-    const item = el.attributes.getNamedItem(ATTR_NAME_RID);
-    return item ? item.value : null;
-  }
-
-  /** Gets a child element by the rid @private */
-  function getChildByRID(el: Element, rid: string, offset = 0): {el: Element, index: number} | null {
-    for (let i = offset; i < el.childElementCount; ++i) {
-      if (getRID(el.children[i]) === rid) {
-        return {el: el.children[i], index: i};
-      }
-    }
-
-    return null;
   }
 }
 
-export default DOMSyncer;
+/**
+ * Embed rendering id (rid) to the HTML elements.
+ *
+ * ```html
+ * <div></div> -> <div data-domsyncerid="0">
+ * ```
+ * @param templateText
+ */
+export function embedRID(templateText: string): string {
+  return templateText.replace(/<([^/%]("(.|\s)*?"|'(.|\s)*?'|<%(.|\s)*?%>|[^"'<])*?)(?<!%)>/g,
+    (m, p1) => `<${p1} ${ATTR_NAME_RID}="${rid++}">`);
+}
+
+/**
+ *
+ * @param nowEl
+ * @param subViewEls
+ * @param newEl
+ */
+export function sync(nowEl: Element, subViewEls: Element[], newEl: Element): void {
+  syncElements(nowEl, subViewEls, newEl);
+}
--- src/scripts/app/util/util.ts
+++ src/scripts/app/util/util.ts
@@ -1,33 +1,44 @@
-namespace Util {
+/**
+ * Sleep by setTimeout
+ * @param sleepMs sleep time
+ * @return Always resolved with no data.
+ */
+export async function sleep(sleepMs: number): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => resolve(), sleepMs);
+  });
+}
 
-  /**
-   * Sleep by setTimeout
-   * @param sleepMs sleep time
-   * @return Always resolved with no data.
-   */
-  export async function sleep(sleepMs: number): Promise<void> {
-    return new Promise<void>((resolve) => {
-      setTimeout(() => resolve(), sleepMs);
-    });
-  }
-
-  export async function $deferred<T>(dfd: any): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      dfd.done((v: T) => resolve(v))
-         .fail((err: any) => reject(err));
-    });
-  }
-
-  export async function ignoreError<T>(promise: Promise<T>, errors?: any[]): Promise<T | void> {
-    try {
-      return await promise;
-    } catch(err) {
-     if (errors) {
-       errors.push(err);
-     }
-     return Promise.resolve(err);
-   }
+/**
+ * Ignore rejected promise.
+ * @param promise Target promise for ignoring
+ *   If this promise is rejected,
+ *   This function ignore error and return resolved promise with error object.
+ * @param errors
+ *   If the promise parameter is rejected,
+ *   rejected object (e.g. error object) is stored this parameter.
+ *   Promise is forced resolbed but you can check by errors.
+ */
+export async function ignoreError<T>(promise: Promise<T>, errors?: any[]): Promise<T | void> {
+  try {
+    return await promise;
+  } catch(err) {
+    if (errors) {
+      errors.push(err);
+    }
+    return Promise.resolve(err);
   }
 }
 
-export default Util;
+/**
+ * I dont't now this function is necessary or not;)
+ * Convert jQuery deffered / promise object to ES2015 promise.
+ *
+ * @param dfd Target jQuery deferred / promise object
+ */
+export async function $d<T>(dfd: any): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    dfd.done((v: T) => resolve(v))
+        .fail((err: any) => reject(err));
+  });
+}
